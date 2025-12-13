d2cc468afed83a351f51b2ab5e5846ede4366851
VechniMetel <3512546747@qq.com>
2025-12-13 16:51:45 +0100

    initialize

---
+++ src/main/java/top/nebula/utils/NebulaUtils.java
@@ -0,0 +1,12 @@
+package top.nebula.utils;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(NebulaUtils.MODID)
+public class NebulaUtils {
+	public static final String MODID = "nebula_utils";
+	public static final String NAME = "Team Nebula Utils";
+	private static final Logger LOGGER = LogUtils.getLogger();
+}
