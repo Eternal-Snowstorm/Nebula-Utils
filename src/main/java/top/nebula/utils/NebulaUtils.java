2808d7b834aa1817f633b1e75771ab077204ee3e
Qi-Month <dy757410@163.com>
2025-12-14 01:21:53 +0800

    main

--- src/main/java/top/nebula/utils/NebulaUtils.java
+++ src/main/java/top/nebula/utils/NebulaUtils.java
@@ -1,19 +1,19 @@
 package top.nebula.utils;
 
-import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import top.nebula.utils.config.CommonConfig;
 
 @Mod(NebulaUtils.MODID)
 public class NebulaUtils {
 	public static final String MODID = "nebula_utils";
 	public static final String NAME = "Team Nebula Utils";
-	private static final Logger LOGGER = LogUtils.getLogger();
+	private static final Logger LOGGER = LogManager.getLogger("Nebula");
 
 	/**
 	 * 使用图腾动画
