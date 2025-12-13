2405d4b4090d64dc70401f33ff22d2e0e3d95324
Qi-Month <dy757410@163.com>
2025-12-14 01:14:58 +0800

    动画

--- src/main/java/top/nebula/utils/NebulaUtils.java
+++ src/main/java/top/nebula/utils/NebulaUtils.java
@@ -1,6 +1,8 @@
 package top.nebula.utils;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 
@@ -9,4 +11,13 @@ public class NebulaUtils {
 	public static final String MODID = "nebula_utils";
 	public static final String NAME = "Team Nebula Utils";
 	private static final Logger LOGGER = LogUtils.getLogger();
+
+	/**
+	 * 使用图腾动画
+	 *
+	 * @param stack 传入物品
+	 */
+	public static void useTotemAnimation(ItemStack stack) {
+		Minecraft.getInstance().gameRenderer.displayItemActivation(stack);
+	}
 }
