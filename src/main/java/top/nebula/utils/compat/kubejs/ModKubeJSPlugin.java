1469236b60bb5430c69ff838058b2abe3ec9ca5d
Qi-Month <dy757410@163.com>
2025-12-14 01:16:24 +0800

    转移

--- src/main/java/top/nebula/utils/compat/kubejs/ModKubeJSPlugin.java
+++ src/main/java/top/nebula/utils/compat/kubejs/ModKubeJSPlugin.java
@@ -2,6 +2,7 @@ package top.nebula.utils.compat.kubejs;
 
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.script.BindingsEvent;
+import top.nebula.utils.NebulaUtils;
 import top.nebula.utils.multiblock.MultiblockStructureBuilder;
 import top.nebula.utils.multiblock.PropertyImmutableMap;
 
@@ -11,6 +12,7 @@ public class ModKubeJSPlugin extends KubeJSPlugin {
 	public void registerBindings(BindingsEvent event) {
 		super.registerBindings(event);
 
+		event.add("NebulaUtils", NebulaUtils.class);
 		event.add("MultiblockStructureBuilder", MultiblockStructureBuilder.class);
 		event.add("PropertyImmutableMap", PropertyImmutableMap.class);
 		event.add("LocalDateTime", LocalDateTime.class);
