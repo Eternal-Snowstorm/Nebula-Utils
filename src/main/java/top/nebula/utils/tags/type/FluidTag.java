62fc0f534383b5de94590847ae272bb9d765295a
Qi-Month <dy757410@163.com>
2026-01-21 00:37:41 +0800

    tagsbuilder

---
+++ src/main/java/top/nebula/utils/tags/type/FluidTag.java
@@ -0,0 +1,17 @@
+package top.nebula.utils.tags.type;
+
+import net.minecraft.tags.FluidTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.material.Fluid;
+import top.nebula.utils.tags.AbstractTagBuilder;
+
+public class FluidTag extends AbstractTagBuilder<Fluid> {
+	public FluidTag(String name) {
+		super(name);
+	}
+
+	@Override
+	public TagKey<Fluid> build() {
+		return FluidTags.create(id());
+	}
+}
---
+++ src/main/java/top/nebula/utils/tags/type/ItemTag.java
@@ -0,0 +1,17 @@
+package top.nebula.utils.tags.type;
+
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import top.nebula.utils.tags.AbstractTagBuilder;
+
+public class ItemTag extends AbstractTagBuilder<Item> {
+	public ItemTag(String name) {
+		super(name);
+	}
+
+	@Override
+	public TagKey<Item> build() {
+		return ItemTags.create(id());
+	}
+}
