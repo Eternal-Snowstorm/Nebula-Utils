ccd7ec915fdfa1cfa3e58a478db6917f064453ff
Eternal-Snowstorm <3512546747@qq.com>
2026-01-25 19:07:36 +0100

    我没招了

--- src/main/java/top/nebula/libs/event/FluidTooltipHandler.java
+++ src/main/java/top/nebula/libs/event/FluidTooltipHandler.java
@@ -1,8 +1,15 @@
 package top.nebula.libs.event;
 
+import com.mojang.datafixers.util.Either;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidUtil;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.ArrayList;
@@ -11,52 +18,62 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 
+@Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public final class FluidTooltipHandler {
 	private static final Map<ResourceLocation, List<Consumer<List<Component>>>> FLUID_HANDLERS = new HashMap<>();
 	private static final Map<ResourceLocation, List<Consumer<List<Component>>>> TAG_HANDLERS = new HashMap<>();
 
-	private FluidTooltipHandler() {
+	public static void registerFluid(ResourceLocation fluidId, Consumer<List<Component>> handler) {
+		FLUID_HANDLERS.computeIfAbsent(fluidId, location -> new ArrayList<>())
+				.add(handler);
 	}
 
-	public static void registerFluid(ResourceLocation id, Consumer<List<Component>> handler) {
-		FLUID_HANDLERS.computeIfAbsent(id, (location) -> {
-			return new ArrayList<>();
-		}).add(handler);
+	public static void registerTag(ResourceLocation fluidTag, Consumer<List<Component>> handler) {
+		TAG_HANDLERS.computeIfAbsent(fluidTag, location -> new ArrayList<>())
+				.add(handler);
 	}
 
-	public static void registerTag(ResourceLocation tag, Consumer<List<Component>> handler) {
-		TAG_HANDLERS.computeIfAbsent(tag, (location) -> {
-			return new ArrayList<>();
-		}).add(handler);
+
+	@SubscribeEvent
+	public static void onGatherTooltip(RenderTooltipEvent.GatherComponents event) {
+
+		ItemStack stack = event.getItemStack();
+		if (stack.isEmpty()) return;
+
+		FluidUtil.getFluidContained(stack).ifPresent(fluid -> {
+
+			List<Component> tooltip = new ArrayList<>();
+
+			for (var e : event.getTooltipElements()) {
+				if (e.left().isPresent()) {
+					tooltip.add(Component.literal(e.left().get().getString()));
+				}
+			}
+
+			fire(fluid, tooltip);
+
+			for (Component c : tooltip) {
+				event.getTooltipElements().add(Either.left(c));
+			}
+		});
 	}
 
-	public static void fire(FluidStack fluid, List<Component> tooltip) {
-		if (fluid.isEmpty()) {
-			return;
-		}
+	private static void fire(FluidStack fluid, List<Component> tooltip) {
+		if (fluid.isEmpty()) return;
 
-		ResourceLocation fluidId = ForgeRegistries.FLUIDS.getKey(fluid.getFluid());
-		if (fluidId == null) {
-			return;
-		}
+		ResourceLocation id = ForgeRegistries.FLUIDS.getKey(fluid.getFluid());
+		if (id == null) return;
 
-		List<Consumer<List<Component>>> list = FLUID_HANDLERS.get(fluidId);
+		var list = FLUID_HANDLERS.get(id);
 		if (list != null) {
-			list.forEach((handler) -> {
-				handler.accept(tooltip);
-			});
+			list.forEach(h -> h.accept(tooltip));
 		}
 
-		fluid.getFluid()
-				.builtInRegistryHolder()
-				.tags()
-				.forEach((tag) -> {
-					List<Consumer<List<Component>>> handlers = TAG_HANDLERS.get(tag.location());
-					if (handlers != null) {
-						handlers.forEach((handler) -> {
-							handler.accept(tooltip);
-						});
-					}
-				});
+		fluid.getFluid().builtInRegistryHolder().tags().forEach(tag -> {
+			var handlers = TAG_HANDLERS.get(tag.location());
+			if (handlers != null) {
+				handlers.forEach(h -> h.accept(tooltip));
+			}
+		});
 	}
 }
