0e3d65e4989bfe2a4928b8fe01dd3cea93be8f30
Qi-Month <dy757410@163.com>
2026-01-31 20:52:50 +0800

    ICheckModLoaded

--- src/main/java/top/nebula/libs/compat/ICheckModLoaded.java
+++ src/main/java/top/nebula/libs/compat/ICheckModLoaded.java
@@ -11,11 +11,19 @@ public interface ICheckModLoaded {
 		return hasMod("create");
 	}
 
+	static boolean hasTCon() {
+		return hasMod("tconstruct");
+	}
+
 	static boolean hasKubeJS() {
 		return hasMod("kubejs");
 	}
 
-	static boolean hasTCon() {
-		return hasMod("tconstruct");
+	static boolean hasJade() {
+		return hasMod("jade");
+	}
+
+	static boolean hasJei() {
+		return hasMod("jei");
 	}
 }
