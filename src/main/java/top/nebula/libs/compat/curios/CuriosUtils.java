b1abf79d5695124589545936a48f6eb6994ea5bf
Qi-Month <dy757410@163.com>
2026-02-15 22:18:50 +0800

    换成LEti好一点

--- src/main/java/top/nebula/libs/compat/curios/CuriosUtils.java
+++ src/main/java/top/nebula/libs/compat/curios/CuriosUtils.java
@@ -1,12 +1,12 @@
 package top.nebula.libs.compat.curios;
 
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import top.theillusivec4.curios.api.CuriosApi;
 
 public class CuriosUtils {
-	public static boolean hasItem(Player player, Item item) {
-		return CuriosApi.getCuriosInventory(player)
+	public static boolean hasItem(LivingEntity entity, Item item) {
+		return CuriosApi.getCuriosInventory(entity)
 				.map((handler) -> {
 					return handler.findFirstCurio((stack) -> {
 						return stack.is(item);
