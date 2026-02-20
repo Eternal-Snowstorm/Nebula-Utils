1278dc2c76034d1f42b284acf0cd85363082e600
xiaoliziawa <3682822069@qq.com>
2026-02-20 18:36:19 +0800

    提交

--- src/main/java/dev/celestiacraft/libs/compat/jade/NebulaJadePlugin.java
+++ src/main/java/dev/celestiacraft/libs/compat/jade/NebulaJadePlugin.java
@@ -8,7 +8,6 @@ import snownee.jade.api.WailaPlugin;
 public class NebulaJadePlugin implements IWailaPlugin {
 	@Override
 	public void registerClient(IWailaClientRegistration registration) {
-		// Priority 10001: runs AFTER KubeJS's callback (10000), so we can post-process their text
 		registration.addTooltipCollectedCallback(10001, CommonJadeTipProvider::onTooltipCollected);
 	}
 }
