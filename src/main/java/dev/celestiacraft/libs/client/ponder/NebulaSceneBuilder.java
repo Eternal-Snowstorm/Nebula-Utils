2bf1ac98e5ee1fede09cba1e67ad270b7b545e93
Qi-Month <dy757410@163.com>
2026-04-16 01:36:20 +0800

    ponder builder 211

---
+++ src/main/java/dev/celestiacraft/libs/client/ponder/NebulaSceneBuilder.java
@@ -0,0 +1,91 @@
+package dev.celestiacraft.libs.client.ponder;
+
+import com.simibubi.create.foundation.ponder.CreateSceneBuilder;
+import net.createmod.ponder.api.element.ElementLink;
+import net.createmod.ponder.api.element.EntityElement;
+import net.createmod.ponder.api.level.PonderLevel;
+import net.createmod.ponder.api.scene.SceneBuilder;
+import net.createmod.ponder.api.scene.SceneBuildingUtil;
+import net.createmod.ponder.api.scene.Selection;
+import net.createmod.ponder.foundation.PonderScene;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+
+public class NebulaSceneBuilder extends CreateSceneBuilder {
+	public static final Object OBJECT = new Object();
+	private PonderScene ponderScene;
+	private final ExtendedWorldInstructions world;
+
+	public NebulaSceneBuilder(SceneBuilder builder) {
+		super(builder);
+		world = new ExtendedWorldInstructions();
+	}
+
+	@Override
+	public @NotNull ExtendedWorldInstructions world() {
+		return world;
+	}
+
+	public void showStructure() {
+		showStructure(ponderScene.getBasePlateSize() * 2);
+	}
+
+	public void showStructure(int height) {
+		BlockPos start = new BlockPos(ponderScene.getBasePlateOffsetX(), 0, ponderScene.getBasePlateOffsetZ());
+		BlockPos size = new BlockPos(ponderScene.getBasePlateSize() - 1, height, ponderScene.getBasePlateSize() - 1);
+		Selection selection = ponderScene.getSceneBuildingUtil().select().cuboid(start, size);
+		encapsulateBounds(size);
+		world().showSection(selection, Direction.UP);
+	}
+
+	public void encapsulateBounds(BlockPos size) {
+		addInstruction((scene) -> {
+			PonderLevel sceneWorld = scene.getWorld();
+			sceneWorld.getBounds().encapsulate(size);
+		});
+	}
+
+	public static void init5x5(SceneBuilder builder, SceneBuildingUtil util) {
+		builder.configureBasePlate(0, 0, 5);
+		builder.scaleSceneView(0.9f);
+		builder.world().showSection(util.select().layer(0), Direction.UP);
+	}
+
+	public static void init7x7(SceneBuilder builder, SceneBuildingUtil util) {
+		builder.configureBasePlate(0, 0, 7);
+		builder.scaleSceneView(0.75f);
+		builder.world().showSection(util.select().layer(0), Direction.UP);
+	}
+
+	public static void init9x9(SceneBuilder builder, SceneBuildingUtil util) {
+		builder.configureBasePlate(0, 0, 9);
+		builder.scaleSceneView(0.6f);
+		builder.world().showSection(util.select().layer(0), Direction.UP);
+	}
+
+	public static void rotateAround(SceneBuilder builder, int duration, int angle) {
+		float times = 360f / angle;
+
+		for (int i = 0; i < times; i++) {
+			rotate(builder, (int) (duration / times), angle);
+		}
+	}
+
+	public static void rotate(SceneBuilder builder, int time, int angle) {
+		builder.rotateCameraY(angle);
+		builder.idle(time);
+	}
+
+	public class ExtendedWorldInstructions extends WorldInstructions {
+		public void removeEntity(ElementLink<EntityElement> link) {
+			addInstruction((scene) -> {
+				EntityElement resolve = scene.resolve(link);
+				if (resolve != null) {
+					resolve.ifPresent(Entity::discard);
+				}
+			});
+		}
+	}
+}
